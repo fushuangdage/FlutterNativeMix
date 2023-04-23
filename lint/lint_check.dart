dc8efaa844d5751fa151a2e3b463a95c3c68c099
fushuang <fushuang.666@bytedance.com>
2023-04-23 15:45:49 +0800

    添加lint 检测代码

---
+++ lint/lint_check.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class ImageNetwork extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: strToColor('#8C8C8C'),
+    );
+  }
+}
+
+/// 把类似#7f7f7f这种颜色转换成color
+Color strToColor(String colorStr, {Color fallback}) {
+  if (colorStr == null) {
+    return fallback;
+  }
+  try {
+    return Color(int.parse(colorStr.replaceAll("#", "0xff")));
+  } catch (e, s) {
+    debugPrint("$e\n$s");
+  }
+  return fallback;
+}
---
+++ lint/main.dart
@@ -0,0 +1,63 @@
+
+import 'dart:io';
+
+import 'package:analyzer/dart/analysis/features.dart';
+import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:args/args.dart';
+
+void main(List<String> arguments) {
+  exitCode = 0; // presume success
+  final parser = ArgParser()..addFlag("file", negatable: false, abbr: 'f');
+
+  var argResults = parser.parse(arguments);
+  final paths = argResults.rest;
+  if (paths.isEmpty) {
+    stdout.writeln('No file found');
+  } else {
+    generate(paths[0]);
+  }
+}
+
+class DemoAstVisitor extends GeneralizingAstVisitor<Map> {
+  @override
+  Map visitNode(AstNode node) {
+    //输出遍历AST Node 节点内容
+    stdout.writeln("${node.runtimeType}<---->${node.toSource()}");
+    return super.visitNode(node);
+  }
+  @override
+  Map visitMethodInvocation(MethodInvocation node) {
+    stdout.write("visitMethodInvocation : ${node.runtimeType} ======== ${node.toSource()}");
+    return super.visitMethodInvocation(node);
+  }
+}
+
+//生成AST
+Future generate(String path) async {
+  if (path.isEmpty) {
+    stdout.writeln("No file found");
+  } else {
+    await _handleError(path);
+    if (exitCode == 2) {
+      try {
+        var parseResult = parseFile(path: path, featureSet: FeatureSet.fromEnableFlags([]));
+        var compilationUnit = parseResult.unit;
+        //遍历AST
+        compilationUnit.accept(DemoAstVisitor());
+      } catch (e) {
+        stdout.writeln('Parse file error: ${e.toString()}');
+      }
+    }
+  }
+}
+
+Future _handleError(String path) async {
+  if (await FileSystemEntity.isDirectory(path)) {
+    stderr.writeln('error: $path is a directory');
+  } else {
+    exitCode = 2;
+  }
+}
+
